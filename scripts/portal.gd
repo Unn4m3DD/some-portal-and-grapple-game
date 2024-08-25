bb3c9496bfe2dac4ad10e7ec70011ac699c39099
Andre Patacas <andre.patacas@morphotech.co.uk>
2024-08-25 17:11:22 +0100

    feat: add 2 way portal

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -2,5 +2,13 @@ extends Area2D
 
 @onready var other: Node2D = get_node("./%s" % get_meta("other"))
 
+static var portal_mutex = false
+
 func _on_body_entered(body: Node2D) -> void:
+	if portal_mutex:
+		return
+	portal_mutex = true
 	body.position = other.global_position
+
+func _on_body_exited(_body: Node2D) -> void:
+	portal_mutex = false
