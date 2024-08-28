f203576634894f71e5f605028f347c7c8605f7b7
TraderOfDreams <mariaabreuneves@gmail.com>
2024-08-28 16:21:35 +0100

    feat: add portal projectile

--- scripts/portal_projectile.gd
+++ scripts/portal_projectile.gd
@@ -4,12 +4,16 @@ extends Area2D
 @onready var portal_manager := $ / root / Level / PortalManager
 @onready var ray_cast_2d := $RayCast2D
 
+const PORTAL_PROJECTILE_BLUE = preload("res://materials/portal_projectile_blue.tres")
+const PORTAL_PROJECTILE_ORANGE = preload("res://materials/portal_projectile_orange.tres")
+
 var direction := Vector2.ZERO
 const speed := 2500.0
 var box_scene := preload("res://scenes/box.tscn")
 var portal_color := ''
 
 func _process(delta: float) -> void:
+	$Projectile2.process_material = PORTAL_PROJECTILE_BLUE if portal_color == "blue" else PORTAL_PROJECTILE_ORANGE
 	position += direction * speed * delta
 	if ray_cast_2d.is_colliding():
 		var other_body: TileMapLayer = ray_cast_2d.get_collider()
