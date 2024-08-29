a1ab46f51f9cec7b32b9a114d4ec6c44020ce5eb
TraderOfDreams <mariaabreuneves@gmail.com>
2024-08-29 18:23:30 +0100

    feat: add door3 particles and animation

--- scripts/door.gd
+++ scripts/door.gd
@@ -3,6 +3,8 @@ extends CollisionObject2D
 @export var button: Node2D;
 @export var is_inverted: bool
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var preview_1: Sprite2D = $Preview1
+@onready var preview_2: Sprite2D = $Preview2
 
 
 func toggle_door_visibility(new_visibility: bool):
@@ -12,6 +14,8 @@ func toggle_door_visibility(new_visibility: bool):
 		animation_player.play("open")
 
 func _ready():
+	preview_1.visible = false
+	preview_2.visible = false
 	(button.is_pressed as Signal).connect(on_button_pressed)
 	(button.is_not_pressed as Signal).connect(on_button_not_pressed)
 	if !is_inverted: 
